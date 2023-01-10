cc74c7134d2cfad2ebf12c92ba01db83bcf1e9db
Iridio <andrescarlos2211@gmail.com>
2023-01-10 20:42:34 -0300

    Agrega catalogo libros

--- js/catalogo.js
+++ js/catalogo.js
@@ -4,42 +4,42 @@ const catalog = [
         nombre:"El Mito de Sisifo, Albert Cammus",
         codigo: "A001",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$5000"  
     },
     {
         imagen:"img/img_2.webp",
         nombre:"Psicología y Alquimia, Carl Jung",
         codigo: "A002",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$5000"  
     },
     {
         imagen:"/img/img_3.jpg",
         nombre:"Vida y obra ilustradas, H.P. Lovecraft",
         codigo: "A003",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$10000"  
     },
     {
         imagen:"img/img_4.jpeg",
         nombre:"El cuervo, Edgard Allan Poe",
         codigo: "A004",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$700"  
     },
     {
         imagen:"/img/img_5.jpg",
         nombre:"Qabalah, Qliphot y Magia Goetica, Thomas Karlsson",
         codigo: "A005",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$6660"  
     },
     {
         imagen:"/img/img_6.webp",
         nombre:"Tree of Qliphot, Asenath Mason",
         codigo: "A006",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$11000"  
     },
     {
         imagen:"/img/img_7.jpg",
@@ -53,14 +53,14 @@ const catalog = [
         nombre:"Yo antes de ti, Jojo Moyes",
         codigo: "A008",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$15000"  
     },
     {
         imagen:"/img/img_9.webp",
         nombre:"Shoot your shot, Vernon Brundage JR",
         codigo: "A009",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$8000"  
     },
     {
         imagen:"/img/img_10.webp",
@@ -74,28 +74,28 @@ const catalog = [
         nombre:"Amazing inventions that changed the World, Bill Oneill",
         codigo: "A011",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$15000"  
     },
     {
         imagen:"/img/img_12.webp",
         nombre:"La magia de la Manifestacion, Ryuu Shinohara",
         codigo: "A012",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$12000"  
     },
     {
         imagen:"/img/img_13.webp",
         nombre:"Mas astuto que el Diablo, Napoleon Hill",
         codigo: "A013",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$15000"  
     },
     {
         imagen:"/img/img_14.webp",
         nombre:"Inteligencia emocional, Fabian Goleman",
         codigo: "A014",
         descripcion: "Gran libro",
-        precio: "$1500"  
+        precio: "$12000"  
     }
 ]
 
