ed169029194538467e3d3df6b4cc1d6edfc7865b
Iridio <andrescarlos2211@gmail.com>
2023-01-10 21:58:51 -0300

    codigo html de productos desde js

--- js/main.js
+++ js/main.js
@@ -7,8 +7,42 @@ $(document).ready(function(){
 });
 
 
-document.getElementById(Cards).innerHTML= codeBlock
+//var codeBlock = '<h1>Holaaa</h1>';
+//document.getElementById("products").innerHTML= codeBlock
 
-var codeBlock = '<section id="Cards">' +
-                '<h1>Holaaa</h1>' +
-                '</section>';
+
+
+const productsBlocks = [];
+
+ const productBlocks = catalog.map( (product) => {
+    return `<div class="col-md-3">
+    <div class="product-block">
+        <img class="d-block w-100 foto" src="${product.imagen}" alt="Product">
+        <h3>${product.nombre}</h3>
+        <p>Codigo ${product.codigo}</p>
+        <p>${product.descripcion}</p>
+        <p>${product.precio}</p>
+        <div class="row">
+            <div class="col-md-6">
+                <button class="btn btn-outline-success btn-sm" type="button" id="boton5" data-id="5"
+                    onclick="add(this)">Añadir</button>
+            </div>
+            <div class="col-md-6">
+                <button class="btn btn-outline-danger btn-sm" type="button" id="btn_det5" data-id="5">Detalles</button>
+            </div>
+        </div>
+    </div>
+</div>`
+});
+
+const productsHTML = '';
+
+for (let i=0; i < productBlocks.length ; i++) {
+    productsHTML = productsHTML + productBlocks[i];
+}
+
+console.log('productsBlocks', productsBlocks);
+console.log('productsHTML', productsHTML);
+
+document.getElementById("products").innerHTML = productsHTML;
+
