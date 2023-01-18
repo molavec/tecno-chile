02a1810220b3bfdae0b666cef3223863af9a7f9e
Iridio <andrescarlos2211@gmail.com>
2023-01-18 20:10:16 -0300

    Modifica boton

---
+++ js/confirmacion.js
@@ -0,0 +1,6 @@
+document.getElementById('total-neto')
+document.getElementById('iva')
+document.getElementById('total')
+document.getElementById('shipping')
+document.getElementById('total-with-shipping')
+
