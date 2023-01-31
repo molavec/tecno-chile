299ba6089c77f7c9bfe4b6c3c9d557104943c94d
Miguel Olave <molavec@gmail.com>
2023-01-30 21:46:44 -0300

    ajustes cosmeticos

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 1. done - Construir prototipo de una web del tipo tienda en línea y que tenga un comportamiento responsivo.
 2. done - Deberá incluir la implementación de Bootstrap e implementar al menos 1 carousel, tooltips, entre otros.
 3. done - Deberá contener un componente Navbar con links que lleven a las diferentes secciones de la web.
-4. Debe tener un footer que incluya iconos de redes sociales,
+4. done - Debe tener un footer que incluya iconos de redes sociales,
 5. Deberá solicitar al usuario nombre y apellido, los cuales deberán ser mostrarlos en alguna sección cliente.
-6. Deberá contener una página de ventas, la cual deberá implementar card de Bootstrap, deberá agregar al carrito de compras el producto seleccionado y luego listar el nombre del producto y su precio.
+6. done - Deberá contener una página de ventas, la cual deberá implementar card de Bootstrap, deberá agregar al carrito de compras el producto seleccionado y luego listar el nombre del producto y su precio.
 7. Para el punto anterior, deberá crear una estructura de datos mediante arreglos.
 8. De acuerdo a la tabla siguiente vas a realizar el arreglo de datos para mostrar en pantalla según lo solicitado
--- js/catalogo.js
+++ js/catalogo.js
@@ -1,4 +1,32 @@
 const catalog = [
+    {
+        imagen: './img/img_01.jpg',
+        nombre: 'Reloj',
+        codigo: '1',
+        descripcion: 'Gran reloj',
+        precio: '300'
+    },
+    {
+        imagen: './img/img_02.jpeg',
+        nombre: 'Audífonos tipo 1',
+        codigo: '2',
+        descripcion: 'Gran audífono',
+        precio: '200'
+    },
+    {
+        imagen: './img/img_03.jpeg',
+        nombre: 'Audífonos tipo 2',
+        codigo: '3',
+        descripcion: 'Gran audífono',
+        precio: '100'
+    },
+    {
+        imagen: './img/img_04.jpg',
+        nombre: 'Silla',
+        codigo: '4',
+        descripcion: 'Gran Silla',
+        precio: '400'
+    },
     // {
     //     nombre: "El gran Gatsby",
     //     imagen: "https://www.imagen.com/el-gran-gatsby.jpg",
@@ -55,102 +83,88 @@ const catalog = [
     //     descripcion: "Novela sobre un hombre que despierta convertido en un insecto",
     //     precio: 19,
     // },
-    {
-        imagen: './img/img_1.jpg',
-        nombre: 'El Mito de Sisifo, Albert Cammus',
-        codigo: 'A001',
-        descripcion: 'Gran libro',
-        precio: '5000'
-    },
-    {
-        imagen: './img/img_2.webp',
-        nombre: 'Psicología y Alquimia, Carl Jung',
-        codigo: 'A002',
-        descripcion: 'Gran libro',
-        precio: '5000'
-    },
-    {
-        imagen: './img/img_3.jpg',
-        nombre: 'Vida y obra ilustradas, H.P. Lovecraft',
-        codigo: 'A003',
-        descripcion: 'Gran libro',
-        precio: '10000'
-    },
-    {
-        imagen: './img/img_4.jpeg',
-        nombre: 'El cuervo, Edgard Allan Poe',
-        codigo: 'A004',
-        descripcion: 'Gran libro',
-        precio: '700'
-    },
-    {
-        imagen: './img/img_5.jpg',
-        nombre: 'Qabalah, Qliphot y Magia Goetica, Thomas Karlsson',
-        codigo: 'A005',
-        descripcion: 'Gran libro',
-        precio: '6660'
-    },
-    {
-        imagen: './img/img_6.webp',
-        nombre: 'Tree of Qliphot, Asenath Mason',
-        codigo: 'A006',
-        descripcion: 'Gran libro',
-        precio: '11000'
-    },
-    {
-        imagen: './img/img_7.jpg',
-        nombre: 'Manual de como no ligar con tu jefe, Kora Clark',
-        codigo: 'A007',
-        descripcion: 'Gran libro',
-        precio: '1500'
-    },
-    {
-        imagen: './img/img_8.webp',
-        nombre: 'Yo antes de ti, Jojo Moyes',
-        codigo: 'A008',
-        descripcion: 'Gran libro',
-        precio: '15000'
-    },
-    {
-        imagen: './img/img_9.webp',
-        nombre: 'Shoot your shot, Vernon Brundage JR',
-        codigo: 'A009',
-        descripcion: 'Gran libro',
-        precio: '8000'
-    },
-    {
-        imagen: './img/img_10.webp',
-        nombre: 'El poder del pensamiento positivo, Normal Vincent Peale',
-        codigo: 'A010',
-        descripcion: 'Gran libro',
-        precio: '1500'
-    },
-    {
-        imagen: './img/img_11.webp',
-        nombre: 'Amazing inventions that changed the World, Bill Oneill',
-        codigo: 'A011',
-        descripcion: 'Gran libro',
-        precio: '15000'
-    },
-    {
-        imagen: './img/img_12.webp',
-        nombre: 'La magia de la Manifestacion, Ryuu Shinohara',
-        codigo: 'A012',
-        descripcion: 'Gran libro',
-        precio: '12000'
-    },
-    {
-        imagen: './img/img_13.webp',
-        nombre: 'Mas astuto que el Diablo, Napoleon Hill',
-        codigo: 'A013',
-        descripcion: 'Gran libro',
-        precio: '15000'
-    },
-    {
-        imagen: './img/img_14.webp',
-        nombre: 'Inteligencia emocional, Fabian Goleman',
-        codigo: 'A014',
-        descripcion: 'Gran libro',
-        precio: '12000'
-    }
+    // {
+    //     imagen: './img/img_3.jpg',
+    //     nombre: 'Vida y obra ilustradas, H.P. Lovecraft',
+    //     codigo: 'A003',
+    //     descripcion: 'Gran libro',
+    //     precio: '10000'
+    // },
+    // {
+    //     imagen: './img/img_4.jpeg',
+    //     nombre: 'El cuervo, Edgard Allan Poe',
+    //     codigo: 'A004',
+    //     descripcion: 'Gran libro',
+    //     precio: '700'
+    // },
+    // {
+    //     imagen: './img/img_5.jpg',
+    //     nombre: 'Qabalah, Qliphot y Magia Goetica, Thomas Karlsson',
+    //     codigo: 'A005',
+    //     descripcion: 'Gran libro',
+    //     precio: '6660'
+    // },
+    // {
+    //     imagen: './img/img_6.webp',
+    //     nombre: 'Tree of Qliphot, Asenath Mason',
+    //     codigo: 'A006',
+    //     descripcion: 'Gran libro',
+    //     precio: '11000'
+    // },
+    // {
+    //     imagen: './img/img_7.jpg',
+    //     nombre: 'Manual de como no ligar con tu jefe, Kora Clark',
+    //     codigo: 'A007',
+    //     descripcion: 'Gran libro',
+    //     precio: '1500'
+    // },
+    // {
+    //     imagen: './img/img_8.webp',
+    //     nombre: 'Yo antes de ti, Jojo Moyes',
+    //     codigo: 'A008',
+    //     descripcion: 'Gran libro',
+    //     precio: '15000'
+    // },
+    // {
+    //     imagen: './img/img_9.webp',
+    //     nombre: 'Shoot your shot, Vernon Brundage JR',
+    //     codigo: 'A009',
+    //     descripcion: 'Gran libro',
+    //     precio: '8000'
+    // },
+    // {
+    //     imagen: './img/img_10.webp',
+    //     nombre: 'El poder del pensamiento positivo, Normal Vincent Peale',
+    //     codigo: 'A010',
+    //     descripcion: 'Gran libro',
+    //     precio: '1500'
+    // },
+    // {
+    //     imagen: './img/img_11.webp',
+    //     nombre: 'Amazing inventions that changed the World, Bill Oneill',
+    //     codigo: 'A011',
+    //     descripcion: 'Gran libro',
+    //     precio: '15000'
+    // },
+    // {
+    //     imagen: './img/img_12.webp',
+    //     nombre: 'La magia de la Manifestacion, Ryuu Shinohara',
+    //     codigo: 'A012',
+    //     descripcion: 'Gran libro',
+    //     precio: '12000'
+    // },
+    // {
+    //     imagen: './img/img_13.webp',
+    //     nombre: 'Mas astuto que el Diablo, Napoleon Hill',
+    //     codigo: 'A013',
+    //     descripcion: 'Gran libro',
+    //     precio: '15000'
+    // },
+    // {
+    //     imagen: './img/img_14.webp',
+    //     nombre: 'Inteligencia emocional, Fabian Goleman',
+    //     codigo: 'A014',
+    //     descripcion: 'Gran libro',
+    //     precio: '12000'
+    // }
 ]
